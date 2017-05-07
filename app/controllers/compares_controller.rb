c8ddec1c8f85155c0c44b307ae71ee406f160cfe
Hiroki Sato <myhome.satoh+github@gmail.com>
2017-05-07 20:49:08 +0900

    計算結果を表示できるように修正

--- app/controllers/compares_controller.rb
+++ app/controllers/compares_controller.rb
@@ -4,7 +4,22 @@ class ComparesController < ApplicationController
   def new
   end
 
+  def index
+    @lblPrice = "価格"
+    @lblCapacity = "容量（ml, g, cc, cmなど）"
+    @lblNumber = "個数"
+    @btnClear = "Clear"
+    @btnCompare = "どっち？"
+  end
+
   def compare
+    # ----------------
+    @lblPrice = "価格"
+    @lblCapacity = "容量（ml, g, cc, cmなど）"
+    @lblNumber = "個数"
+    @btnClear = "Clear"
+    @btnCompare = "どっち？"
+    # ----------------
     @leftPrice = params[:leftPrice]
     @leftCapacity = params[:leftCapacity]
     @leftNumber = params[:leftNumber]
@@ -12,8 +27,21 @@ class ComparesController < ApplicationController
     @rightCapacity = params[:rightCapacity]
     @rightNumber = params[:rightNumber]
 
-    leftResult = CommonUtil.how_match_per_piece(@leftPrice, @leftCapacity, @leftNumber)
-    rightResult = CommonUtil.how_match_per_piece(@rightPrice, @rightCapacity, @rightNumber)
+    if @leftNumber.empty?
+      leftResult = how_match_per_piece(@leftPrice, @leftCapacity)
+    else
+      leftResult = how_match_per_piece(@leftPrice, @leftCapacity, @leftNumber)
+    end
+
+    if @rightNumber.empty?
+      rightResult = how_match_per_piece(@rightPrice, @rightCapacity)
+    else
+      rightResult = how_match_per_piece(@rightPrice, @rightCapacity, @rightNumber)
+    end
+
+    @leftPerCapacity = leftResult
+    @rightPerCapacity = rightResult
     @is_left = leftResult < rightResult
+    @is_right = rightResult < leftResult
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'compares#login'
+  root 'compares#index'
   get 'compares' => 'compares#index'
   post 'compares/compare' => 'compares#compare'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
